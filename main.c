686e49df691961232715c51b4d9e200206334286
soumia <sekhriasoumi@gmail.com>
2024-01-07 02:51:06 +0100

    modification sur l'enregistrement etudiant

--- main.c
+++ main.c
@@ -7,25 +7,47 @@ typedef struct {
       int nbreng;             //le nombre d'enregistrements
 }enteteF;
 
+
+ // enregistrement physique avec chevauchement
 typedef struct{
+      int sizeofnom;
       char nom[50];
+      int sizeofprenom;
       char prenom[50];
+      int sizeofmatricule;
       int matricule;
+      char fineng;
+      char finchamps;
 }etudiant;
+
+
+
+
 void ecrireng(FILE *projetsfsd, etudiant *etu)  // procédure qui écrit un enregistrement physique dans le fichier
  {
       fwrite(etu, sizeof(etudiant),1,projetsfsd);
  }
 
 
+
 int main()
- {    FILE *projetsfsd= fopen(projetsfsd,"w");
+ {   char fineng='#';             // mark fin d'enregistrement
+     char finchamps='$';          // mark fin de champs
+
+
+
+     FILE *projetsfsd= fopen(projetsfsd,"w");
+
+
+
       //l'initialisation de l'entete
       enteteF entete;
       nbrblock=3;
       nbreng=5;
 
       fwrite(&entete, sizeof(enteteF),1,projetsfsf);
+
+
 
 
 
