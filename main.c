c2f9b7f3b402b3d933ba0db0d4418bff99b0588e
soumia <sekhriasoumi@gmail.com>
2024-01-07 01:50:29 +0100

    procédure d'ecriture d'enregistrement

--- main.c
+++ main.c
@@ -12,7 +12,14 @@ typedef struct{
       char nom[50];
       char prenom[50];
       int matricule;
-}
+}etudiant
+
+
+void ecrireng(FILE *projetsfsd, etudiant *etu)  // procédure qui écrit un enregistrement physique dans le fichier
+ {
+      fwrite(etu; sizeof(etudiant),1,projetsfsd);
+ }
+
 
 
 
