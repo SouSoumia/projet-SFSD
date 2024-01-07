cc3903baa3e9a4bfc8cf2514df04b375756fe0fc
soumia <sekhriasoumi@gmail.com>
2024-01-07 01:28:35 +0100

    création de l'entete

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+      int nbrblock; //le nombre de blocks
+      int 4;        //le nombre d'enregistrements
+}entete
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main()
 {
-    printf("hello world");
+
     return 0;
 }
