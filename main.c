9c298e50f3bb9183f1ade368d8cbfbc433f77809
soumia <sekhriasoumi@gmail.com>
2024-01-12 02:06:23 +0100

    des procédures de lecture et écriture en utilisant le buffer

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-*************************************************création de fichier*************************************************************
+#define max 100
+//*************************************************création de fichier*************************************************************
   // enregistrement physique
   typedef struct etudiant
 {
@@ -21,8 +22,7 @@
 typedef block buffer;
 
 typedef struct entete
-{   int nbrblock;                      //le nombre de blocks
-    int nbreng;                       //le nombre d'enregistrements
+{
     int No_dern_bloc;                // indice de dernier bloc
     int NB_enreg_inser;             // nombre d'enregistrement insérié
     int NB_enreg_suppr;            // nombre d'enregistrement supprimé
@@ -40,28 +40,58 @@ typedef struct entete
     if (mode == 'a')                             // ajouter ou ouvrir en mode ajout
     {
         f->fichier = fopen(nom, "rb+");
-        fread(&(f->entety), sizeof(struct entete), 1, f->fichier);
+        fread(&(f->entetef), sizeof(struct entete), 1, f->fichier);
     }
     else
     {
         if (mode == 'n')                         //créer un nouveau fichier ou ouvrir en mode création
         {
             f->fichier = fopen(nom, "wb+");
-            f->entety.No_dern_bloc = 1;
-            f->entety.ind_pos_libre = 0;
-            f->entety.NB_enreg_inser = 0;
-            f->entety.NB_enreg_suppr = 0;
+            f->entetef.No_dern_bloc = 1;
+            f->entetef.NB_enreg_inser = 0;
+            f->entetef.NB_enreg_suppr = 0;
         }
     }
     return f;
 }
+    void fermer(sfsdtovc *f)
+{
+    rewind(f->fichier);                                           // positionner le curseur au debut du fichier
+    fwrite(&(f->entetef), sizeof(struct entete), 1, f->fichier);  // Écrit l'entête du fichier (entety) dans le fichier
+    fclose(f->fichier);
+    free(f);                                               // Libère la mémoire allouée dynamiquement pour la structure sfsdtovc
+}
 
+// lire un bloc spécifique à partir d'un fichier  et le stocker dans le buffer (buf est un pointeur vers le buffer)
+void lire(sfsdtovc *f, int num_block, buffer *buf)
+{
+    if (num_block <= (f->entetef).No_dern_bloc)
+    {
+        fseek(f->fichier, sizeof(entete) + (num_block - 1) * sizeof(struct block), SEEK_SET); //(SEEK_SET) qui définit le début du fichier comme référence.
+        fread(buf, sizeof(struct block), 1, f->fichier);
+    }
+}
+// écrire le contenu d'un tampon (buffer) dans un bloc spécifique
 
+void ecrire(sfsdtovc *f, int num_block, buffer *buf)
+{
+    if (num_block <= (f->entetef).No_dern_bloc)
+    {
+        fseek(f->fichier, sizeof(entete) + (num_block - 1) * sizeof(struct block), SEEK_SET);
+        fwrite(buf, sizeof(struct block), 1, f->fichier);
+    }
+}
 
+// La fonction est conçue pour retourner différentes valeurs de l'entête du fichier en fonction de la valeur de i
 
-
-
-
+int entete_f(sfsdtovc *f, int i)
+{
+    if (i == 1)
+        return f->entetef.No_dern_bloc;
+    if (i == 2)
+        return f->entetef.NB_enreg_inser;
+    if (i == 3)
+        return f->entetef.NB_enreg_suppr;
 
 
 
