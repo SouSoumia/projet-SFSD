36834571c17ed0180538a4b54c703971fdf15b97
lmn69exe <bensalah.ibtihal2005@gmail.com>
2024-01-23 09:26:08 +0100

    adding the binary search algorithm

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define max 100
-//*************************************************création de fichier*************************************************************
+//*************************************************crï¿½ation de fichier*************************************************************
   // enregistrement physique
   typedef struct etudiant
 {
@@ -24,8 +24,8 @@ typedef block buffer;
 typedef struct entete
 {
     int No_dern_bloc;                // indice de dernier bloc
-    int NB_enreg_inser;             // nombre d'enregistrement insérié
-    int NB_enreg_suppr;            // nombre d'enregistrement supprimé
+    int NB_enreg_inser;             // nombre d'enregistrement insï¿½riï¿½
+    int NB_enreg_suppr;            // nombre d'enregistrement supprimï¿½
     int ind_pos_libre;            // indice de position libre
 } entete;
 
@@ -45,7 +45,7 @@ typedef struct entete
     }
     else
     {
-        if (mode == 'n')                         //créer un nouveau fichier ou ouvrir en mode création
+        if (mode == 'n')                         //crï¿½er un nouveau fichier ou ouvrir en mode crï¿½ation
         {
             f->fichier = fopen(nom, "wb+");
             f->entetef.No_dern_bloc = 1;
@@ -59,21 +59,21 @@ typedef struct entete
     void fermer(sfsdtovc *f)
 {
     rewind(f->fichier);                                           // positionner le curseur au debut du fichier
-    fwrite(&(f->entetef), sizeof(struct entete), 1, f->fichier);  // Écrit l'entête du fichier (entety) dans le fichier
+    fwrite(&(f->entetef), sizeof(struct entete), 1, f->fichier);  // ï¿½crit l'entï¿½te du fichier (entety) dans le fichier
     fclose(f->fichier);
-    free(f);                                               // Libère la mémoire allouée dynamiquement pour la structure sfsdtovc
+    free(f);                                               // Libï¿½re la mï¿½moire allouï¿½e dynamiquement pour la structure sfsdtovc
 }
 
-// lire un bloc spécifique à partir d'un fichier  et le stocker dans le buffer (buf est un pointeur vers le buffer)
+// lire un bloc spï¿½cifique ï¿½ partir d'un fichier  et le stocker dans le buffer (buf est un pointeur vers le buffer)
 void lire(sfsdtovc *f, int num_block, buffer *buf)
 {
     if (num_block <= (f->entetef).No_dern_bloc)
     {
-        fseek(f->fichier, sizeof(entete) + (num_block - 1) * sizeof(struct block), SEEK_SET); //(SEEK_SET) qui définit le début du fichier comme référence.
+        fseek(f->fichier, sizeof(entete) + (num_block - 1) * sizeof(struct block), SEEK_SET); //(SEEK_SET) qui dï¿½finit le dï¿½but du fichier comme rï¿½fï¿½rence.
         fread(buf, sizeof(struct block), 1, f->fichier);
     }
 }
-// écrire le contenu d'un tampon (buffer) dans un bloc spécifique
+// ï¿½crire le contenu d'un tampon (buffer) dans un bloc spï¿½cifique
 
 void ecrire(sfsdtovc *f, int num_block, buffer *buf)
 {
@@ -84,7 +84,7 @@ void ecrire(sfsdtovc *f, int num_block, buffer *buf)
     }
 }
 
-// La fonction est conçue pour retourner différentes valeurs de l'entête du fichier en fonction de la valeur de i
+// La fonction est conï¿½ue pour retourner diffï¿½rentes valeurs de l'entï¿½te du fichier en fonction de la valeur de i
 
 int entete_f(sfsdtovc *f, int i)
 {
@@ -97,7 +97,7 @@ int entete_f(sfsdtovc *f, int i)
     if (i == 4)
         return f->entetef.NB_enreg_suppr;
 
-  //cette procédure permet de modifier différentes valeurs de l'entête fonction de la valeur de i
+  //cette procï¿½dure permet de modifier diffï¿½rentes valeurs de l'entï¿½te fonction de la valeur de i
 void aff_entete(sfsdtovc *f, int i, int val)
 {
     if (i == 1)
@@ -107,9 +107,9 @@ void aff_entete(sfsdtovc *f, int i, int val)
     if (i == 3)
         f->entetef.NB_enreg_inser = val;
     if (i == 4)
-        f->entetef.NB_enreg_suppr = val; // Si i est égal à 4 la procédure met à jour la valeur de NB_enreg_suppr dans la structure entetef de la structure tovc avec la valeur fournie (val).
+        f->entetef.NB_enreg_suppr = val; // Si i est ï¿½gal ï¿½ 4 la procï¿½dure met ï¿½ jour la valeur de NB_enreg_suppr dans la structure entetef de la structure tovc avec la valeur fournie (val).
 }
-// llouer un nouveau bloc dans le fichier on va l'utiliser aprés
+// llouer un nouveau bloc dans le fichier on va l'utiliser aprï¿½s
 int alloc_block(sfsdtovc *f)
 {
     aff_entete(f, 1, entete_f(f, 1) + 1);
@@ -117,20 +117,20 @@ int alloc_block(sfsdtovc *f)
     return entete_f(f, 1);
 
 
-// parcourir tous les blocs du fichier et les afficher et lit le contenu de chaque bloc du fichier dans un buffer et exécute potentiellement des opérations sur les données du buffer
+// parcourir tous les blocs du fichier et les afficher et lit le contenu de chaque bloc du fichier dans un buffer et exï¿½cute potentiellement des opï¿½rations sur les donnï¿½es du buffer
 
 void afficher(sfsdtovc *f)
 {
-    int i = 1, j;                                      //i à 1 pour représenter le numéro du bloc en cours d'examen
+    int i = 1, j;                                      //i ï¿½ 1 pour reprï¿½senter le numï¿½ro du bloc en cours d'examen
     buffer buf;
     while (i <= (entete_f(f, 1)))
     {
         j = 0;
-        lire(f, i, &buf);     //pour lire le contenu du bloc numéro i du fichier dans le buffer (buf).
+        lire(f, i, &buf);     //pour lire le contenu du bloc numï¿½ro i du fichier dans le buffer (buf).
         while (j < max)
         {
         }
-        i++;   //Incrementer le numéro du bloc (i) pour passer au bloc suivant lors de la prochaine itération de la boucle principale.
+        i++;   //Incrementer le numï¿½ro du bloc (i) pour passer au bloc suivant lors de la prochaine itï¿½ration de la boucle principale.
     }
 
 
@@ -147,7 +147,7 @@ void afficher(sfsdtovc *f)
      scanf("%s", etud.nom);
 
 
-    printf("Saisir le prénom : ");
+    printf("Saisir le prï¿½nom : ");
     scanf("%s", etud.prenom);
 
 
@@ -160,33 +160,65 @@ void afficher(sfsdtovc *f)
 
 
 
-     // Demander à l'utilisateur le numéro du bloc où insérer l'étudiant
+     // Demander ï¿½ l'utilisateur le numï¿½ro du bloc oï¿½ insï¿½rer l'ï¿½tudiant
 
 
-       printf("Saisir le numéro du bloc pour l'insertion : ");
+       printf("Saisir le numï¿½ro du bloc pour l'insertion : ");
        scanf("%d", &num_bloc);
 
 
 
         if (num_bloc > f->entetef.No_dern_bloc) {
-        printf("Le bloc spécifié n'existe pas. L'insertion a échoué.\n");
+        printf("Le bloc spï¿½cifiï¿½ n'existe pas. L'insertion a ï¿½chouï¿½.\n");
         return;
     }
 
-    // lecture et écriture d'aprés le buffer
+    // lecture et ï¿½criture d'aprï¿½s le buffer
       lire(f, num_bloc, &buf);
       ecrire(f, num_bloc, &buf);
 
-      // Mettre à jour les informations dans l'entête
+      // Mettre ï¿½ jour les informations dans l'entï¿½te
 
        aff_entete(f, 2, f->entetef.ind_pos_libre + 1);
        aff_entete(f, 3, f->entetef.NB_enreg_inser + 1);
 
-       printf("Insertion réussie.\n");
+       printf("Insertion rï¿½ussie.\n");
 }
 
 
+// Binary search for the info by the matricule, the returned value is the index of the block where the info is found, the index of the block where the info is found, and a var trouve to indicate if the info is found or not
 
+void Binary_Search(sfsdtovc *f, int matricule, int *num_bloc , int *num_eng , int *trouve)
+{
+    buffer buf;
+    int i = 1, j = 0;
+    int bi = 1, bs = entete_f(f, 1);
+    *trouve = 0; // 0 = false
+    *num_bloc = 0;
+    *num_eng = 0;
+
+    while(bi <= bs && *trouve == 0)
+    {
+        i = (bi + bs) / 2;
+        lire(f, i, &buf);
+        while (j < max && *trouve == 0)
+        {
+            if (buf.tab[j] == matricule)
+            {
+                *trouve = 1;
+                *num_bloc = i;
+                *num_eng = j;
+            }
+            j++;
+        }
+        if (buf.tab[j] > matricule)
+            bs = i - 1;
+        else
+            bi = i + 1;
+    }
+    
+    
+}
 
 
 
